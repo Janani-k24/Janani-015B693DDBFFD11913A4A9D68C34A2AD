5655d295dec69cc3e60ec27f746039b599526cd7
Janani-k24 <144361459+Janani-k24@users.noreply.github.com>
2023-09-16 14:25:45 +0530

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,7 +1,7 @@
-def isLeaYear(year):
+def isLeapYear(year):
   if(year%4==0 and year % 100!=0) or year % 400==0:
     return True
-else:
+    else:
     return False
 year=2020
 if  isLeapYear(year):
